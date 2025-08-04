950ff3b811b6a50c7fb2f1551b796d87becbe9ff
Shubham Phad <shubhamphad.techdev96@gmail.com>
2025-08-04 19:01:04 +0530

    -minor fix

--- app/src/main/java/com/example/stocksportfolioapp_test/data/repository/HoldingsRepositoryImpl.kt
+++ app/src/main/java/com/example/stocksportfolioapp_test/data/repository/HoldingsRepositoryImpl.kt
@@ -2,13 +2,13 @@ package com.example.stocksportfolioapp_test.data.repository
 
 import com.example.stocksportfolioapp_test.data.local_db.HoldingsDao
 import com.example.stocksportfolioapp_test.data.remote.api.HoldingsApi
-import com.example.stocksportfolioapp_test.data.remote.dto.HoldingsListDto
-import com.example.stocksportfolioapp_test.data.remote.dto.UserHoldingDto
 import com.example.stocksportfolioapp_test.data.remote.mapper.toDomain
 import com.example.stocksportfolioapp_test.data.remote.mapper.toEntity
 import com.example.stocksportfolioapp_test.domain.model.UserHoldings
 import com.example.stocksportfolioapp_test.domain.repository.HoldingsRepository
 import com.example.stocksportfolioapp_test.domain.util.Results
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class HoldingsRepositoryImpl(
     private val holdingsApi: HoldingsApi,
@@ -22,18 +22,24 @@ class HoldingsRepositoryImpl(
                 if (response.isSuccessful && response.body() != null) {
                     val remoteData =
                         response.body()?.data?.userHolding?.map { it.toEntity() } ?: emptyList()
-                    holdingsDao.delete()
-                    if (remoteData.isNotEmpty()) {
-                        holdingsDao.insertAll(remoteData)
+                    withContext(Dispatchers.IO) {
+                        holdingsDao.delete()
+                        if (remoteData.isNotEmpty()) {
+                            holdingsDao.insertAll(remoteData)
+                        }
                     }
                 }
             }
 
-            val localData = holdingsDao.getAll().map { it.toDomain() }
+            val localData = withContext(Dispatchers.IO) {
+                holdingsDao.getAll().map { it.toDomain() }
+            }
             Results.Success(localData)
 
         } catch (e: Exception) {
-            val fallbackData = holdingsDao.getAll().map { it.toDomain() }
+            val fallbackData = withContext(Dispatchers.IO) {
+                holdingsDao.getAll().map { it.toDomain() }
+            }
             if (fallbackData.isNotEmpty()) {
                 Results.Success(fallbackData)
             } else {
